4c7a932f357d4242664f1a378ca8e3d2e7e943bb
Ferranlao <ferranlao@hotmail.com>
2019-02-28 17:38:49 +0100

    deplay

--- client/src/lib/Events.js
+++ client/src/lib/Events.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/events",
+    baseURL:  process.env.NODE_ENV === "production" ? "":"http://localhost:3000/events",
     timeout:2000,
     withCredentials: true
   });

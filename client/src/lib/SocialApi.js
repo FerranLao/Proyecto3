f69fc8ea59671665b5ae2b5e412b889dd75675fa
Ferranlao <ferranlao@hotmail.com>
2019-02-28 17:44:12 +0100

    pushito

--- client/src/lib/SocialApi.js
+++ client/src/lib/SocialApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  process.env.NODE_ENV === "production" ? "": "http://localhost:3000/social",
+  baseURL:  process.env.NODE_ENV === "production" ? "/social": "http://localhost:3000/social",
   timeout: 2000,
   withCredentials: true
 });
